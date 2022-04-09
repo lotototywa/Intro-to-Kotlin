91dd36103260f4f2479e5a3a7e914595609a7f7d
Jacek Piekarczyk <43182595+lotototywa@users.noreply.github.com>
2022-04-09 19:09:31 +0200

    Update BirthdayMessage.kts

--- BirthdayMessage.kts
+++ BirthdayMessage.kts
@@ -1,15 +1,51 @@
-fun main() {
+fun main(){
     val name = "Jacek"
     val border = "="
     val timesToRepeat = 21 + name.length
+    val age = 11
+    val layers = 4
     printBorder(border, timesToRepeat)
     println("  Happy Birthday, ${name}!  ")
     printBorder(border, timesToRepeat)
+    println("")
+    printCakeCandles(age)
+    printCakeTop(age)
+	printCakeBottom(age, layers)
 }
 
 fun printBorder(border: String, timesToRepeat: Int) {
     repeat(timesToRepeat) {
         print(border)
+    }
+    println()
+}
+
+fun printCakeCandles(age:Int){
+    print(" ")
+    repeat(age){
+    	print(",")   
+    }
+    println()
+    print(" ")
+    repeat(age){
+    	print("|")   
+    }
+    println()
+}
+
+fun printCakeTop(age:Int){
+	repeat(age + 2){
+        print("=")
+    }
+    println()
+}
+
+fun printCakeBottom(age:Int, layers:Int){
+	repeat(layers){
+        repeat(age + 2){
+            print("@")
+        }
+        println()
     }
     println()
 }
